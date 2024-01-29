22087a36002add0af16111423d23989de2bfcff2
Daniel Jensen <4144311+danto7@users.noreply.github.com>
2024-01-29 22:46:52 +0100

    initial

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/danto7/project-detector
+
+go 1.21.4
+
+require github.com/deckarep/golang-set/v2 v2.6.0 // indirect
---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+
+	mapset "github.com/deckarep/golang-set/v2"
+)
+
+var suffixList = []string{
+	"Cargo.toml",
+	".git",
+	"package.json",
+}
+var projectPaths = mapset.NewSet[string]()
+
+func main() {
+	var pathname string
+	if len(os.Args[1:]) == 1 {
+		pathname = os.Args[1]
+		if pathname == "-h" || pathname == "--help" {
+			usage()
+			os.Exit(0)
+		}
+
+		var err error
+		pathname, err = filepath.Abs(pathname)
+		if err != nil {
+			log.Panicln(err)
+		}
+		fileInfo, err := os.Stat(pathname)
+		if err != nil {
+			log.Panicln(err)
+		}
+		if !fileInfo.IsDir() {
+			usage()
+			os.Exit(1)
+		}
+	} else {
+		usage()
+		os.Exit(1)
+	}
+
+	fileSystem := os.DirFS(pathname)
+
+	fs.WalkDir(fileSystem, ".", func(subPath string, d fs.DirEntry, err error) error {
+		for _, suffix := range suffixList {
+			if strings.HasSuffix(subPath, suffix) {
+				parentPath := path.Join(pathname, path.Dir(subPath))
+				if !projectPaths.Contains(parentPath) {
+					fmt.Println(parentPath)
+				}
+
+				projectPaths.Add(parentPath)
+				return fs.SkipDir
+			}
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		return nil
+	})
+}
+
+func usage() {
+	fmt.Printf(`
+%s <path>
+	<path>		path that should be searched for potential project directorys
+`, os.Args[0])
+}
